2bd7dfacffb6a4c846db6bb3092c4c97c8cc71f3
Suban05 <anatolyb94@gmail.com>
2024-12-21 16:02:25 +0500

    demo data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,31 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+users = []
+states = Bulletin.aasm.states.map(&:name)
+
+5.times do
+  users << User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+end
+
+10.times do
+  category = Category.find_or_create_by(name: Faker::Commerce.department)
+  bulletin_image = ['bulletin1.jpg', 'bulletin2.jpg', 'bulletin3.jpg', 'bulletin4.jpg', 'bulletin5.jpg'].map do |file_path|
+    Rails.root.join("test/fixtures/files/#{file_path}")
+  end
+
+  15.times do
+    bulletin = users.sample.bulletins.build(
+      description: Faker::Lorem.paragraph,
+      title: Faker::Lorem.sentence(word_count: 3),
+      category:,
+      state: states.sample
+    )
+    bulletin.image.attach(io: File.open(bulletin_image.sample), filename: 'filename.jpg')
+    bulletin.save
+  end
+end

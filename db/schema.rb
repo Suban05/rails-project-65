ea64cb77fd9bbd318c73fe9b3ceae2a78959a7f5
Suban05 <anatolyb94@gmail.com>
2024-12-21 16:24:21 +0500

    add indexes

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 20_241_220_165_300) do
+ActiveRecord::Schema[7.2].define(version: 20_241_221_111_807) do
   create_table 'active_storage_attachments', force: :cascade do |t|
     t.string 'name', null: false
     t.string 'record_type', null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.2].define(version: 20_241_220_165_300) do
     t.string 'name'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['name'], name: 'index_categories_on_name', unique: true
   end
 
   create_table 'users', force: :cascade do |t|
@@ -65,6 +66,7 @@ ActiveRecord::Schema[7.2].define(version: 20_241_220_165_300) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.boolean 'admin', default: false, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
   add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'

5bab4d8674cb153fdbbc6f7e598a81877c9be7b2
Suban05 <anatolyb94@gmail.com>
2024-12-21 15:59:30 +0500

    translates

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+ENV['RANSACK_FORM_BUILDER'] = '::SimpleForm::FormBuilder'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +13,8 @@ module RailsProject65
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    config.i18n.default_locale = :ru
+    config.active_storage.variant_processor = :mini_magick
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -25,5 +28,15 @@ module RailsProject65
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_view.sanitized_allowed_tags = %w[
+      strong em b i p code pre tt samp kbd var sub sup dfn cite big small address
+      hr br div span h1 h2 h3 h4 h5 h6 ul ol li dl dt dd abbr acronym a img
+      blockquote del ins table tr td th thead tbody tfoot
+    ]
+
+    config.action_view.sanitized_allowed_attributes = %w[
+      href src width height alt cite datetime title class name xml:lang abbr style
+      target rel data-method data-confirm
+    ]
   end
 end

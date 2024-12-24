abfac6884035bd3dd88665b57eddd3bac6b61d64
Suban05 <anatolyb94@gmail.com>
2024-12-24 16:19:51 +0500

    all users should be admins like in the original project

---
+++ db/migrate/20241224111503_all_users_are_admins.rb
@@ -0,0 +1,6 @@
+class AllUsersAreAdmins < ActiveRecord::Migration[7.2]
+  def change
+    change_column_default :users, :admin, from: nil, to: true
+    change_column_null :users, :admin, false
+  end
+end
